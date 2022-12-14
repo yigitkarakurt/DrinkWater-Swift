746f39d887fdcf3b5cd1bbbe27318b5f924db535
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-12-14 15:51:57 +0300

    Initial Commit

---
+++ DrinkWater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkWater
+//
+//  Created by Yiğit Karakurt on 14.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
