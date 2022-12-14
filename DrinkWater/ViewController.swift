d9efedbf7573417605e6470ae3aec98451c9dcba
yigitkarakurt <yigitkarakurt35@gmail.com>
2022-12-14 16:29:32 +0300

    design - 1

--- DrinkWater/ViewController.swift
+++ DrinkWater/ViewController.swift
@@ -16,4 +16,15 @@ class ViewController: UIViewController {
 
 
 }
-
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
